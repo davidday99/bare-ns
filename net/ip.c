e64f76ce224391ee6fd87f32845aa1269d0fa047
David Day <david.day@utexas.edu>
2021-11-26 19:49:18 -0600

    added more functions

--- net/ip.c
+++ net/ip.c
@@ -2,3 +2,38 @@
 
 static struct ip_pkt rx_buffer[IP_RX_BUF_LEN];
 static struct ip_pkt tx_buffer[IP_TX_BUF_LEN];
+
+static uint8_t rxrdptr;
+static uint8_t rxwrptr;
+static uint8_t txrdptr;
+static uint8_t txwrptr;
+
+uint8_t write_rx_pkt(struct enet_frame *e) {
+    if (rxwrptr < rxrdptr)
+        return 0;
+
+    uint16_t i = 0;
+
+    for (uint8_t j = 0; j < 4; i++, j++)
+        rx_buffer[rxwrptr].dest[j] = e->data[i];
+
+    for (uint8_t j = 0; j < 4; i++, j++)
+        rx_buffer[rxwrptr].src[j] = e->data[i]; 
+
+    // TODO: remaining IP packet fields
+
+    rxwrptr = (rxwrptr + 1) % IP_RX_BUF_LEN;
+
+    return 1;
+}
+
+struct ip_pkt *read_rx_pkt() {
+    struct ip_pkt *ptr;
+    if (rxrdptr == rxwrptr) {
+        ptr = 0;
+    } else {
+        ptr = &rx_buffer[rxrdptr];
+        rxrdptr = (rxrdptr + 1) % IP_RX_BUF_LEN;
+    }
+    return ptr;
+}
--- net/ip.h
+++ net/ip.h
@@ -2,6 +2,7 @@
 #define _IP_H_
 
 #include <stdint.h>
+#include "ethernet.h"
 
 #define IP_RX_BUF_LEN 10
 #define IP_TX_BUF_LEN 10
@@ -13,5 +14,8 @@ struct ip_pkt {
     uint8_t *payload;
     uint16_t plen;
 };
+
+uint8_t write_rx_pkt(struct enet_frame *e);
+struct ip_pkt *read_rx_pkt();
 
 #endif /* _IP_H_ */
