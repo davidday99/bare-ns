e17b1b9ae7bb464b7ae02ce942693af9922692d3
David Day <david.day@utexas.edu>
2021-12-24 18:43:54 -0600

    add functions to get options and data lengths

--- inc/ipv4.h
+++ inc/ipv4.h
@@ -3,6 +3,30 @@
 
 #include <stdint.h>
 
+#define IP_VERSION_4 0x04
+
+#define IPV4_PRECEDENCE_ROUTINE 0
+#define IPV4_PRECEDENCE_PRIORITY 0x20
+#define IPV4_PRECEDENCE_IMMEDIATE 0x40
+#define IPV4_PRECEDENCE_FLASH 0x60
+#define IPV4_PRECEDENCE_FLASH_OVERRIDE 0x80
+#define IPV4_PRECEDENCE_CRITICAL 0xA0
+#define IPV4_PRECEDENCE_INTERNETWORK_CTRL 0xC0
+#define IPV4_PRECEDENCE_NETWORK_CTRL 0xE0
+#define IPV4_PRECEDENC_M 0xE0
+
+#define IPV4_TOS_DELAY_NORMAL 0
+#define IPV4_TOS_THROUGHPUT_NORMAL 0
+#define IPV4_TOS_RELIABILITY_NORMAL 0
+#define IPV4_TOS_DELAY_LOW 0x80
+#define IPV4_TOS_THROUGHPUT_HIGH 0x10
+#define IPV4_TOS_RELIABILITY_HIGH 0x20
+#define IPV4_TOS_M 0x1F
+
+#define IPV4_OPTIONS_OFFSET 20
+
+#define IPV4_MIN_HEADER_LEN 20
+
 struct ipv4hdr {
     uint8_t ihl : 4;
     uint8_t version : 4;
@@ -17,6 +41,7 @@ struct ipv4hdr {
     uint32_t dest;
 };
 
-void icmp_reply(struct ipv4hdr *hdr);
+uint8_t ipv4_options_len(struct ipv4hdr *hdr);
+uint8_t ipv4_data_len(struct ipv4hdr *hdr);
 
 #endif /* _IPV4_H_ */
--- src/ipv4.c
+++ src/ipv4.c
@@ -1,4 +1,6 @@
 #include <stdint.h>
+#include "ipv4.h"
+#include "netcommon.h"
 
 uint8_t itos(uint32_t val, char *buf) {
     uint32_t i = 0;
@@ -32,4 +34,16 @@ void int_to_ipv4(uint32_t ip, char *ipv4) {
     octet = ip & 0xFF;
     ipv4 += itos(octet, ipv4);
     *ipv4++ = '\0';
+}
+
+uint8_t ipv4_options_len(struct ipv4hdr *hdr) {
+    uint8_t len = hdr->ihl - (IPV4_MIN_HEADER_LEN / 4);
+    len *= 4;  // convert from 32-bit fields to bytes
+    return len;
+}
+
+uint8_t ipv4_data_len(struct ipv4hdr *hdr) {
+    uint8_t header_len = hdr->ihl * 4;  // convert from 32-bit fields to bytes
+    uint16_t total_len = hton16(hdr->len);
+    return total_len - header_len;
 }
