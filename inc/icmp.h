02482e136a3fe74af54822ddacb1dc8a9b71103e
David Day <david.day@utexas.edu>
2021-12-26 03:37:39 -0600

    add request, reply, and checksum functions

--- inc/icmp.h
+++ inc/icmp.h
@@ -3,13 +3,22 @@
 
 #include <stdint.h>
 
-struct icmphdr {
+#define ICMP_TYPE_ECHO_REPLY 0
+#define ICMP_TYPE_DEST_UNREACHABLE 3
+#define ICMP_TYPE_ECHO 8
+
+#define ICMP_ECHO_DATA_OFFSET 8
+
+struct icmp_echohdr {
     uint8_t type;
     uint8_t code;
     uint16_t chksm;
+    uint16_t id;
+    uint16_t seqnum;
 };
 
-void icmp_request(struct icmphdr *hdr);
-void icmp_reply(struct icmphdr *hdr);
+void icmp_request(struct icmp_echohdr *hdr);
+void icmp_reply(struct icmp_echohdr *hdr);
+uint16_t icmp_calculate_checksum(uint8_t *data, uint16_t len);
 
 #endif /* _ICMP_H_ */
--- src/icmp.c
+++ src/icmp.c
@@ -1,9 +1,32 @@
 #include "icmp.h"
 
-void icmp_request(struct icmphdr *hdr) {
+static uint16_t icmp_sum_ones_complement(uint8_t *data, uint16_t len);
+
+void icmp_request(struct icmp_echohdr *hdr) {
+    hdr->type = ICMP_TYPE_ECHO;
+    hdr->code = 0;
     return;
 }
 
-void icmp_reply(struct icmphdr *hdr) {
+void icmp_reply(struct icmp_echohdr *hdr) {
     return;
+}
+
+uint16_t icmp_calculate_checksum(uint8_t *data, uint16_t len) {
+    uint16_t sum = icmp_sum_ones_complement(data, len);
+    return ~sum;
+}
+
+static uint16_t icmp_sum_ones_complement(uint8_t *data, uint16_t len) {
+    uint16_t *hwordptr = (uint16_t *) data;
+    uint32_t chksm = 0;
+    uint8_t odd = len & 1;
+    for (uint16_t i = 0; i < (len - odd) / 2; i++) {
+        chksm += *hwordptr++;
+    }
+    if (odd) {
+        chksm += (uint8_t) *hwordptr << 8;
+    }
+    chksm += (chksm >> 16) & 0xFFFF;
+    return (uint16_t) chksm;
 }
