58faa56815154d56d710477ad8a234e93b8390fb
David Day <david.day@utexas.edu>
2022-01-13 04:52:21 -0600

    consolidate *_in and *_out header files into single header file for each module

--- inc/ipv4_in.h
+++
@@ -1,9 +0,0 @@
-#ifndef _IPV4_IN_H_
-#define _IPV4_IN_H_
-
-#include <stdint.h>
-#include "ipv4.h"
-
-void ipv4_deliver(struct ipv4hdr *pkt);
-
-#endif /* _IPV4_IN_H_ */
--- inc/tcp_in.h
+++
@@ -1,8 +0,0 @@
-#ifndef _TCP_IN_H_
-#define _TCP_IN_H_
-
-#include <stdint.h>
-
-void tcp_deliver(uint8_t *payload, uint32_t srcip, uint32_t destip, uint16_t len);
-
-#endif /* _TCP_IN_H_ */
