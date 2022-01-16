eaeeb2580560aa1483abaa06dcc8b636c32c14e5
David Day <david.day@utexas.edu>
2022-01-16 17:22:39 -0600

    add optional MAC parameter to net_tx

--- inc/net.h
+++ inc/net.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h>
 
-__attribute__ ((weak)) void net_tx(uint8_t *data, uint16_t len, uint16_t type);
+__attribute__ ((weak)) void net_tx(uint8_t *destmac, uint8_t *data, uint16_t len, uint16_t type);
 __attribute__ ((weak)) void net_rx(uint8_t *buf);
 
 #endif /* _NET_H_ */
--- src/ipv4_out.c
+++ src/ipv4_out.c
@@ -17,8 +17,8 @@ void ipv4_send(uint32_t destip, uint8_t *data, uint16_t len, uint8_t protcol) {
     hdr->ttl = 64;
     hdr->protocol = protcol;
     hdr->cksm = 0;
-    hdr->dest = hton32(destip);
+    hdr->dest = destip;
     hdr->src = hton32(ipv4_get_address());
     memcpy(ipv4pkt + IPV4_MIN_HEADER_LEN, data, len);
-    net_tx(ipv4pkt, sizeof(ipv4pkt), ETHERTYPE_IPV4);
+    net_tx(0, ipv4pkt, sizeof(ipv4pkt), ETHERTYPE_IPV4);
 }
