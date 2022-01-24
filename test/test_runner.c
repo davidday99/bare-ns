a6828cd855d2d411fb076e6db92a7d8044a16fcf
David Day <david.day@utexas.edu>
2022-01-24 14:50:59 -0600

    delete incomplete/unused icmp module

--- test/test_runner.c
+++ test/test_runner.c
@@ -11,7 +11,6 @@ uint8_t assert(uint32_t expected, uint32_t actual, char *message) {
 int test_ethernet();
 int test_arp();
 int test_ip();
-int test_icmp();
 int test_socket();
 int test_ipv4_to_udp_to_socket();
 int test_socket_send();
@@ -22,7 +21,6 @@ int main() {
     assert(test_ethernet(), 1, "Failed test_ethernet");
     assert(test_arp(), 1, "Failed test_arp");
     assert(test_ip(), 1, "Failed test_ip");
-    assert(test_icmp(), 1, "Failed test_icmp");
     assert(test_socket(), 1, "Failed test_socket");
     assert(test_ipv4_to_udp_to_socket(), 1, "Failed test_ipv4_to_udp_to_socket");
     assert(test_socket_send(), 1, "Failed test_socket_send");
