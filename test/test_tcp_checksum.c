9e1e1262526f0158ef5fc9cc61c40011553b6f6b
David Day <david.day@utexas.edu>
2022-01-27 04:18:01 -0600

    add extra checksum test

--- test/test_tcp_checksum.c
+++ test/test_tcp_checksum.c
@@ -31,6 +31,21 @@ static uint8_t tcp_pseudohdr2[] = {
     0x15  // size
 };
 
+static uint8_t tcp_pseudohdr3[] = {
+    0xC0,
+    0xA8,
+    0x00,
+    0x6F,  // Src ip, 192.168.0.111
+    0xC0,
+    0xA8,
+    0x00,
+    0x81,  // Dest ip, 192168.0.129
+    0x00,  // zero pad
+    0x06,
+    0x00,
+    0x36  // size
+};
+
 static uint8_t tcp_seg1[] = {
     0xC0,
     0xC1,  // src port
@@ -80,6 +95,45 @@ static uint8_t tcp_seg2[] = {
     0xAB,  // 1 byte of data
 };
 
+static uint8_t tcp_seg3[] = {
+    0x1F,
+    0x40,
+    0x9E,
+    0x22,
+    0x00,
+    0x00,
+    0x00,
+    0x01,
+    0x39,
+    0xC3,
+    0x5D,
+    0xC9,
+    0x50,
+    0x18,
+    0x04,
+    0x00,
+    0x00,
+    0x00,
+    0x00,
+    0x00,
+    0x48,
+    0x65,
+    0x6c,
+    0x6c,
+    0x6f,
+    0x20,
+    0x54,
+    0x43,
+    0x50,
+    0x20,
+    0x53,
+    0x65,
+    0x72,
+    0x76,
+    0x65,
+    0x72
+};
+
 
 
 int test_tcp_checksum() {
@@ -87,6 +141,7 @@ int test_tcp_checksum() {
 
     struct tcphdr *hdr1 = (struct tcphdr *) tcp_seg1;
     struct tcphdr *hdr2 = (struct tcphdr *) tcp_seg2;
+    struct tcphdr *hdr3 = (struct tcphdr *) tcp_seg3;
 
     hdr1->cksm = calculate_tcp_checksum(tcp_seg1, tcp_pseudohdr1);
 
@@ -96,7 +151,12 @@ int test_tcp_checksum() {
 
     uint16_t verify_checksum2 = calculate_tcp_checksum(tcp_seg2, tcp_pseudohdr2);
 
+    hdr3->cksm = calculate_tcp_checksum(tcp_seg3, tcp_pseudohdr3);
+
+    uint16_t verify_checksum3 = calculate_tcp_checksum(tcp_seg3, tcp_pseudohdr3);
+
     success &= verify_checksum1 == 0;
     success &= verify_checksum2 == 0;
+    success &= verify_checksum3 == 0;
     return success;
 }
