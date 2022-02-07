a22a111927a077caeb5f0947a3bc7d1f050dfc49
David Day <david.day@utexas.edu>
2022-02-06 18:02:42 -0600

    add more functionality to http library

--- inc/http1_0.h
+++ inc/http1_0.h
@@ -2,8 +2,18 @@
 #define _HTTP_1_0_H_
 
 #include <stdint.h>
+#include "socket.h"
 
-#define MAX_URL_LEN 255
+#define MAX_URI_LEN 255
+#define MAX_DATE_LEN 32
+#define MAX_PRAGMA_LEN 1
+#define MAX_AUTHORIZATION_LEN 1
+#define MAX_FROM_LEN 1
+#define MAX_IF_MODIFIED_SINCE_LEN 1
+#define MAX_REFERER_LEN 1
+#define MAX_USER_AGENT_LEN 1
+
+#define MAX_RESPONSE_LEN 1000
 
 enum http_method {
     GET,
@@ -15,22 +25,56 @@ enum http_method {
 
 typedef uint16_t http_status_code;
 
+struct http_general_header {
+    char *date;
+    char *pragma;
+};
+
+struct http_request_header {
+    char *auth;
+    char *from;
+    char *if_modified_since;
+    char *referer;
+    char *user_agent;
+};
+
+struct http_response_header {
+    char *location;
+    char *server;
+    char *www_auth;
+};
+
+struct http_entity_header {
+    char *allow;
+    char *encoding;
+    char *length;
+    char *expires;
+    char *last_modified;
+};
+
 struct http_request_message {
+    char version[2];
+    struct http_general_header genhdr;
+    struct http_request_header reqhdr;
+    struct http_entity_header entityhdr;
     enum http_method method;
-    // header
-    char url[MAX_URL_LEN];
+    char *uri;
     char *body;
 };
 
 struct http_response_message {
+    char version[2];
     http_status_code status_code;
-    // header
+    struct http_general_header genhdr;
+    struct http_response_header reshdr;
+    struct http_entity_header entityhdr;
     char *body;
 };
 
-enum http_status_code http_request(char *url, enum http_method method, char *body, struct http_response_message *response);
-enum http_status_code http_request_ip(uint32_t ip, enum http_method method, char *body, struct http_response_message *response);
-void http_response(http_status_code status_code, char *body);
+http_status_code http_request(char *url, enum http_method method, char *body, struct http_response_message *response);
+http_status_code http_request_ip(uint32_t ip, enum http_method method, char *body, struct http_response_message *response);
+void http_respond(struct socket *sock, http_status_code status_code, char *location, char *body);
+uint8_t http_parse_request(uint8_t *buf, struct http_request_message *req);
 
 
 #endif /* _HTTP_1_0_H_ */
--- src/application_layer/http1_0.c
+++ src/application_layer/http1_0.c
@@ -1,3 +1,195 @@
 #include "http1_0.h"
 #include <stdint.h>
+#include "string.h"
 
+#define DELIMITER "\r\n"
+
+static uint8_t *parse_request_line(uint8_t *buf, struct http_request_message *req);
+static uint8_t *parse_request_method(uint8_t *buf, struct http_request_message *req);
+static uint8_t *parse_request_uri(uint8_t *buf, struct http_request_message *req);
+static uint8_t *parse_version(uint8_t *buf, struct http_request_message *req);
+
+static uint8_t *set_status_line(uint8_t *buf, struct http_response_message *res);
+static uint8_t *set_response_headers(uint8_t *buf, char *location);
+static uint8_t *set_entity_headers(uint8_t *buf, struct http_response_message *res);
+static uint8_t *append_entity_body(uint8_t *buf, char *body);
+const char *get_reason_phrase(http_status_code status_code);
+
+uint8_t itos(uint32_t val, char *buf);
+
+http_status_code http_request(char *url, enum http_method method, char *body, struct http_response_message *response) {
+    return (http_status_code) 200;
+}
+
+http_status_code http_request_ip(uint32_t ip, enum http_method method, char *body, struct http_response_message *response) {
+    return (http_status_code) 200;
+}
+
+void http_respond(struct socket *s, http_status_code status_code, char *location, char *body) {
+    uint8_t response[MAX_RESPONSE_LEN];
+    uint8_t *bufptr = response;
+    struct http_response_message res;
+    res.version[0] = '1';
+    res.version[1] = '0';
+    res.status_code = status_code;
+    res.body = body;
+    
+    bufptr = set_status_line(response, &res);
+
+    // set general headers -- ignore for now
+
+    // set response headers
+    bufptr = set_response_headers(bufptr, location);
+
+    // set entity headers
+    bufptr = set_entity_headers(bufptr, &res);
+
+    // append entity body, if any
+    if (body != 0)
+        bufptr = append_entity_body(bufptr, body);
+
+    uint16_t response_len = bufptr - response;
+
+    socket_send(s, response, response_len);
+}
+
+static uint8_t *set_status_line(uint8_t *buf, struct http_response_message *res) {
+    char sc_string[4];
+    itos(res->status_code, sc_string);
+    const char *reason_phrase = get_reason_phrase(res->status_code);
+
+    memcpy(buf, "HTTP/", 5);
+    buf += 5;
+    *buf++ = res->version[0];
+    *buf++ = '.';
+    *buf++ = res->version[1];
+    *buf++ = ' ';
+
+    memcpy(buf, sc_string, 3);
+    buf += 3;
+    *buf++ = ' ';
+
+    strcpy(buf, reason_phrase);
+    buf += strlen(reason_phrase);
+    *buf++ = '\r';
+    *buf++ = '\n';
+
+    return buf;
+}
+
+static uint8_t *set_response_headers(uint8_t *buf, char *location) {
+    memcpy(buf, "Location: ", 10);
+    buf += 10;
+    strcpy(buf, location);
+    buf += strlen(location);
+    *buf++ = '\r';
+    *buf++ = '\n';
+    return buf;
+}
+
+static uint8_t *set_entity_headers(uint8_t *buf, struct http_response_message *res) {
+    uint16_t body_len = strlen(res->body);
+    char len_string[6];
+    itos(body_len, len_string);
+
+    memcpy(buf, "Content-Type: text/html\r\n", 25);
+    buf += 25;
+    memcpy(buf, "Content-Length: ", 16);
+    buf += 16;
+    strcpy(buf, len_string);
+    buf += strlen(len_string);
+    *buf++ = '\r';
+    *buf++ = '\n';
+    return buf;
+}
+
+static uint8_t *append_entity_body(uint8_t *buf, char *body) {
+    uint16_t body_len = strlen(body);
+    *buf++ = '\r';
+    *buf++ = '\n';
+    memcpy(buf, body, body_len);
+    buf += body_len;
+    return buf;
+}
+
+// TODO: handle malformed requests
+uint8_t http_parse_request(uint8_t *buf, struct http_request_message *req) {
+    uint8_t *bufptr = buf;
+    bufptr = parse_request_line(buf, req);
+    // parse general headers
+    // parse request headers
+    // parse entity headers
+
+    return 1;  // return 1 if valid request, else return 0
+}
+
+static uint8_t *parse_request_line(uint8_t *buf, struct http_request_message *req) {
+    uint8_t *bufptr = buf;
+    bufptr = parse_request_method(bufptr, req);
+    bufptr = parse_request_uri(bufptr, req);
+    bufptr = parse_version(bufptr, req);
+
+    while (memcmp(bufptr, DELIMITER, 2) != 0)
+        bufptr++;
+
+    return bufptr;
+}
+
+static uint8_t *parse_request_method(uint8_t *buf, struct http_request_message *req) {
+    char method[5];
+    uint8_t i = 0;
+
+    while (*buf == ' ' && *buf == '\t') {
+        *buf++;
+    }
+    while (*buf != ' ' && *buf != '\t') {
+        method[i++] = *buf++;
+    }
+    *buf++ = '\0';
+    method[i] = '\0';
+
+    if (strcmp(method, "GET") == 0) {
+        req->method = GET;
+    } else if (strcmp(method, "HEAD") == 0) {
+        req->method = HEAD;
+    } else if (strcmp(method, "POST") == 0) {
+        req->method = POST;
+    } else if (strcmp(method, "UPDATE") == 0) {
+        req->method = UPDATE;
+    }
+
+    return buf;
+}
+
+static uint8_t *parse_request_uri(uint8_t *buf, struct http_request_message *req) {
+    while (*buf == ' ' && *buf == '\t') {
+        *buf++;
+    }
+    req->uri = buf;
+    while (*buf != ' ' && *buf != '\t') {
+        *buf++;
+    }
+    *buf++ = '\0';
+
+    return buf;
+}
+
+static uint8_t *parse_version(uint8_t *buf, struct http_request_message *req) {
+    while (*buf == ' ' && *buf == '\t') {
+        *buf++;
+    }
+
+    while (*buf++ != '/')
+        ;
+
+    req->version[0] = *buf;
+    buf += 2;  // skip the decimal
+    req->version[1] = *buf++;
+
+    return buf;
+}
+
+const char *get_reason_phrase(http_status_code status_code) {
+    if (status_code == 200)
+        return "OK";
+}
