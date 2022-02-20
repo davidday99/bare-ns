31d000240492735c181b8189f5c07f43f86c09e5
David Day <david.day@utexas.edu>
2022-02-20 01:59:16 -0600

    convert target ip to host endianness as that's what function expects

--- src/data_link_layer/arp.c
+++ src/data_link_layer/arp.c
@@ -45,5 +45,5 @@ void arp_deliver(uint8_t *buf) {
     if (hton32(hdr->ptarget) != ipv4_get_address())
         return;
     if (hton16(hdr->opcode) == ARP_OP_REQUEST)
-        arp_send_reply(MAC, ipv4_get_address(), hdr->hwsender, hdr->psender);
+        arp_send_reply(MAC, hton32(hdr->ptarget), hdr->hwsender, hton32(hdr->psender));
 }
