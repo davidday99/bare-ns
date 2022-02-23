22bd6dbefdfcca07c1de85eca531c92b36aa928d
David Day <david.day@utexas.edu>
2022-02-23 00:57:49 -0600

    add broadcast mac constant and gateway mac global

--- src/net_interface.c
+++ src/net_interface.c
@@ -1,5 +1,8 @@
 #include <stdint.h>
 
+const uint8_t MAC_BROADCAST = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
+uint8_t GATEWAY_MAC[6] = {0,0,0,0,0,0};
+
 __attribute__ ((weak)) void net_tx(uint8_t *destmac, uint8_t *data, uint16_t len, uint16_t type) {
     return;
 }
@@ -11,6 +14,3 @@ __attribute__ ((weak)) void net_rx(uint8_t *buf) {
 __attribute__ ((weak)) void net_mac_address(uint8_t *buf) {
     return;
 }
-
-
-
