a37f620773838651f00f62475206786b92e76b5d
David Day <david.day@utexas.edu>
2022-02-23 19:51:23 -0600

    declare destmac param as const and define MAC_BROADCAST as array (previously a single int, bug)

--- inc/net.h
+++ inc/net.h
@@ -6,7 +6,7 @@
 extern const uint8_t MAC_BROADCAST[];
 extern uint8_t GATEWAY_MAC[];
 
-__attribute__ ((weak)) void net_tx(uint8_t *destmac, uint8_t *data, uint16_t len, uint16_t type);
+__attribute__ ((weak)) void net_tx(const uint8_t *destmac, uint8_t *data, uint16_t len, uint16_t type);
 __attribute__ ((weak)) void net_rx(uint8_t *buf);
 __attribute__ ((weak)) void net_mac_address(uint8_t *buf);
 
--- src/net_interface.c
+++ src/net_interface.c
@@ -1,9 +1,9 @@
 #include <stdint.h>
 
-const uint8_t MAC_BROADCAST = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
+const uint8_t MAC_BROADCAST[] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
 uint8_t GATEWAY_MAC[6] = {0,0,0,0,0,0};
 
-__attribute__ ((weak)) void net_tx(uint8_t *destmac, uint8_t *data, uint16_t len, uint16_t type) {
+__attribute__ ((weak)) void net_tx(const uint8_t *destmac, uint8_t *data, uint16_t len, uint16_t type) {
     return;
 }
 
